192fbae291694754ea01526942bae236f0705abf
Karen Cassani Patiño <karen@Karens-MacBook-Air.local>
2026-04-20 20:00:13 -0600

    changes in class

--- index.js
+++ index.js
@@ -2,34 +2,82 @@ import express from 'express';
 import { disconnect } from 'node:cluster';
 import { createServer } from 'node:http';
 import { Server } from 'socket.io';
+import pg from "pg"
 
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
-    cors:'http://localhost:5173/',
+  cors: {
+    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    methods: ['GET', 'POST'],
+    credentials: true
+  }
 });
+
+
+// PostgreSQL connection
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+// create our 'messages' table (you can ignore the 'client_offset' column for now)
+await pool.query(`
+  CREATE TABLE IF NOT EXISTS messages (
+      id SERIAL PRIMARY KEY,
+      client_offset TEXT UNIQUE,
+      content TEXT
+  );
+`);
+
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world3</h1>');
+  res.send('<h1>Hello world 3</h1>');
 });
 
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
   console.log('a user connected', socket.id);
-  socket.on('disconnect', () => {
-    console.log('user disconnected', socket.id);
-  });
+  if (!socket.recovered) {
+      try {
+        const result = await pool.query(
+          'SELECT id, content FROM messages WHERE id > $1 ORDER BY id',
+          [socket.handshake.auth.serverOffset || 0]
+        );
+        
+        for (const row of result.rows) {
+          socket.emit('chat message', row.content, row.id);
+        }
+      } catch (e) {
+        console.error('Error fetching messages:', e);
+      }
+    }
 
-    socket.on('chat message', (msg) => {
+ socket.on('chat message', async (msg) => {
     console.log('message: ' + msg);
+    let result;
+    try {
+      result = await pool.query(
+        'INSERT INTO messages (content) VALUES ($1) RETURNING id',
+        [msg]
+      );
+      // include the offset with the message
+      io.emit('chat message', msg, result.rows[0].id);
+    } catch (e) {
+      console.error('Error inserting message:', e);
+      return;
+    }
   });
 
-socket.on('disconnect', () => {
-    console.log('Usser disconnected', socket.id)
-})
+  socket.on('disconnect', () => {
+    console.log('User disconnected', socket.id);
+  });
+  connectionStateRecovery: {
 
+  }
 });
 
-server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+// prep for deployment
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, '0.0.0.0', () => {
+  console.log('server running on port ${PORT}');
 });
